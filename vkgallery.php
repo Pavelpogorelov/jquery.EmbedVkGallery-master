5413ad8c6bf0cf5fa511e8e468e47d8eecbec71e
Pavelpogorelov <38319412+Pavelpogorelov@users.noreply.github.com>
2018-07-25 11:05:47 +0300

    Update vkgallery.php

--- vkgallery.php
+++ vkgallery.php
@@ -2,8 +2,8 @@
 
 require_once('./vendor/autoload.php');
 
-define('TOKEN', '86529292f5c1c666f4899193a83ac143876cd717b308a085799e66bca4cb109cc086dd776d20986092c71');
-$_GET['access_token'] = TOKEN;
+define('TOKEN', 'ba5e6d21ba5e6d21ba5e6d213bba3b2f5dbba5eba5e6d21e11008e57a7e5cc4a05236bf');
+$_GET['86529292f5c1c666f4899193a83ac143876cd717b308a085799e66bca4cb109cc086dd776d20986092c71'] = TOKEN;
 $params = http_build_query($_GET);
 $url = 'https://api.vk.com/method/photos.get?&' . $params;
 
